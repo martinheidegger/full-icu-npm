dd8186d473ccaa92ebd89dcd51fcf8da2f02aea4
Martin Heidegger <martin.heidegger@gmail.com>
2021-02-12 18:00:50 +0900

    windows support

--- node-full-icu.js
+++ node-full-icu.js
@@ -5,5 +5,11 @@ const env = data.icu_small ? {
   ...process.env,
   NODE_ICU_DATA: data.datPath()
 } : process.env
+const isWin = process.platform === 'win32'
+const cmd = isWin ? 'node' : '/usr/bin/env'
+const args = process.argv.slice(2)
+if (!isWin) {
+  args.unshift('node')
+}
 
-spawn('/usr/bin/env', ['node', ...process.argv.slice(2)], { env, stdio: 'inherit' })
+spawn(cmd, args, { env, stdio: 'inherit' })
